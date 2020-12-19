91fe76b8204d874e1e0df0a557b4302611a9bdfa
Alexander Briones <alexanderbvart@gmail.com>
2020-12-19 12:25:58 -0500

    + mostrar informacion de los proyectos, model factory: item y tipo  | - factory de items, tipo item

--- resources/views/project.blade.php
+++ resources/views/project.blade.php
@@ -3,33 +3,132 @@
 @section('content')
     
 <section class="container mx-auto px-4">
-    {{$project->name}} <br>
-    {{$project->description}} <br>
+    {{$project->name}} <br><br>
+    {{$project->description}} <br><br>
     
-    mision <br>
-    {{$project->info->mission}}
+    Mision de {{$project->name}} <br>
+    "{{$project->info->mission}}".
     <br> <br>
-    vision <br>
-    {{$project->info->vision}}
+    Vision de {{$project->name}}  <br>
+    "{{$project->info->vision}}".
     <br> <br>
-    Propuesta de valor<br>
+    Propuesta de valor de {{$project->name}} <br>
     {{$project->info->valueProposition}}
     <br> <br>
-    Factor diferenciador <br>
+    Factor diferenciador de {{$project->name}}  <br>
     {{$project->info->differentiatingFactor}}
     <br> <br>
-{{--     
-    Objetivos <br>
+    
+   {{--  {{$project->info->objetivos}} --}}
     <?php 
-    $objs = $project->info->objetivos
-    
+        $objs = $project->info->objetivos;
+        $items =$project->itemfodas;
     ?>
-    <br> <br>
-     @foreach ( $objs as $obj)
-    - {{$obj->description}} <br>
+
+
+    @if ($objs->count())
+        <br>
+            Objetivos de {{$project->name}}
+        <br> 
+        @foreach ( $objs as $obj)
+        - {{$obj->description}} <br>
+        
+        @endforeach  
+    @else
+        no hay objetivos :D
+    @endif
+
+{{-- todos los items
+    @if ($items->count())
+        items de {{$project->name}}
+        <br> <br>
+        @foreach ( $items as $item)
+        - {{$item->description}} <br>
+
+        @endforeach  
+    @else
+        no hay objetivos :D
+    @endif --}}
+
+{{--"tipoitem_id"     
+    1 fortaleza
+    2 oportunidades
+    3 debilidades
+    4 amenazas --}}
+
+    @if ($items->count())
+        <br><br>
+        Fortalezas de {{$project->name}}
+        <br>
+        @foreach ( $items as $item)
+
+            @if ($item->tipoitem_id == 1)
+            - {{$item->description}} ||| {{$item->tipoitem_id}} <br>
+{{--             @else
+            estos no son <br> --}}
+            @endif
+
+        @endforeach  
+    @else
+        no hay fortalezas :D
+    @endif
+
+
+
+
+    @if ($items->count())
+        <br><br>
+        Oportunidades de {{$project->name}}
+        <br>
+        @foreach ( $items as $item)
+
+            @if ($item->tipoitem_id == 2)
+            - {{$item->description}} ||| {{$item->tipoitem_id}} <br>
+{{--             @else
+            estos no son <br> --}}
+            @endif
+
+        @endforeach  
+    @else
+        no hay oportunidades :D
+    @endif
     
-    @endforeach  --}}
-    
+
+
+    @if ($items->count())
+        <br><br>
+        Debilidades de {{$project->name}}
+        <br>
+        @foreach ( $items as $item)
+
+            @if ($item->tipoitem_id == 3)
+            - {{$item->description}} ||| {{$item->tipoitem_id}} <br>
+{{--             @else
+            estos no son <br> --}}
+            @endif
+
+        @endforeach  
+    @else
+        no hay debilidades, somos muy fuertes :D
+    @endif
+
+
+    @if ($items->count())
+        <br><br>
+        Amenazas de {{$project->name}}
+        <br>
+        @foreach ( $items as $item)
+
+            @if ($item->tipoitem_id == 4)
+            - {{$item->description}} ||| {{$item->tipoitem_id}} <br>
+{{--             @else
+            estos no son <br> --}}
+            @endif
+
+        @endforeach  
+    @else
+        nadie nos amenaza, nosotros somos la amennaza :D
+    @endif
 
 </section>
 
