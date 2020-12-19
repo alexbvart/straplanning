3ab0d20a39f84edbff8f680f4c9736b277007098
Alexander Briones <alexanderbvart@gmail.com>
2020-12-19 18:50:37 -0500

    antes de agregar | - factory de items, tipo item

--- resources/views/project.blade.php
+++ resources/views/project.blade.php
@@ -3,36 +3,63 @@
 @section('content')
     
 <section class="container mx-auto px-4">
-    {{$project->name}} <br><br>
-    {{$project->description}} <br><br>
+
+    <h1 class="title-font font-bold sm:text-4xl text-3xl mb-2 mt-8  text-gray-900">
+        {{$project->name}} 
+    </h1>
+    <p class="mb-4 leading-relaxed">
+        {{$project->description}} <br><br>
+    </p>
     
-    Mision de {{$project->name}} <br>
-    "{{$project->info->mission}}".
-    <br> <br>
-    Vision de {{$project->name}}  <br>
-    "{{$project->info->vision}}".
-    <br> <br>
-    Propuesta de valor de {{$project->name}} <br>
-    {{$project->info->valueProposition}}
-    <br> <br>
-    Factor diferenciador de {{$project->name}}  <br>
-    {{$project->info->differentiatingFactor}}
-    <br> <br>
+    <h2 class="title-font sm:text-2xl text-1xl mb-2 font-medium text-gray-900">
+        Mision de {{$project->name}}
+    </h2>
+    <p class="mb-6 leading-relaxed">
+        "{{$project->info->mission}}".
+    </p>
+    
+    <h2 class="title-font sm:text-2xl text-1xl mb-2 font-medium text-gray-900">
+        Vision de {{$project->name}}
+    </h2>
+    <p class="mb-6 leading-relaxed">
+        "{{$project->info->vision}}".
+    </p>
+    
+    <h2 class="title-font sm:text-2xl text-1xl mb-2 font-medium text-gray-900">
+        Propuesta de valor de {{$project->name}}
+    </h2>
+    <p class="mb-6 leading-relaxed">
+        {{$project->info->valueProposition}}
+    </p>
+
+    <h2 class="title-font sm:text-2xl text-1xl mb-2 font-medium text-gray-900">
+        Factor diferenciador de {{$project->name}} 
+    </h2>
+    <p class="mb-6 leading-relaxed">
+        {{$project->info->differentiatingFactor}}
+    </p>
+
     
    {{--  {{$project->info->objetivos}} --}}
     <?php 
         $objs = $project->info->objetivos;
         $items =$project->itemfodas;
     ?>
+    
+        
 
+       
 
     @if ($objs->count())
-        <br>
+        <h2 class="title-font sm:text-2xl text-1xl mb-2 font-medium text-gray-900">
             Objetivos de {{$project->name}}
-        <br> 
+        </h2>
         @foreach ( $objs as $obj)
-        - {{$obj->description}} <br>
-        
+        <ul>
+            <li>
+                - {{$obj->description}} <br>
+            </li>
+        </ul>
         @endforeach  
     @else
         no hay objetivos :D
@@ -54,107 +81,111 @@
     1 fortaleza
     2 oportunidades
     3 debilidades
-    4 amenazas --}}
+    4 amenazas
+    shadow rounded my --}}
+    <div class="flex flex-wrap mx-auto bg-red-100 rounded ">
 
-    @if ($items->count())
-        <div class="rounded bg-grey-light flex-no-shrink w-full  sm:w-1/2 p-4 my-4 bg-blue-100">
+        @if ($items->count())
+            <div class="   bg-grey-light w-full  sm:w-1/2 p-4 bg-blue-100">
+                <div class="flex justify-between py-1">
+                    <h3 class="text-sm">
+                        Fortalezas de {{$project->name}}
+                    </h3>
+                </div>
+                
+                <ol class="text-sm mt-2">
+                @foreach ( $items as $item)
+
+                    @if ($item->tipoitem_id == 1)
+                    <li class="bg-white p-2 rounded mt-1 border-b border-grey cursor-pointer hover:bg-grey-lighter">
+                        {{$item->description}} {{-- || {{$item->tipoitem_id}} <br> --}}
+                    </li>
+                    @endif
+
+                @endforeach     
+                </ol>
+            </div>
+        @else
+            no hay fortalezas :D
+        @endif
+
+
+
+        @if ($items->count())
+        <div class=" bg-grey-light  w-full  sm:w-1/2 p-4  bg-red-100">
             <div class="flex justify-between py-1">
                 <h3 class="text-sm">
-                    Fortalezas de {{$project->name}}
+                    Oportunidades de {{$project->name}}
                 </h3>
             </div>
-            
             <ol class="text-sm mt-2">
             @foreach ( $items as $item)
 
-                @if ($item->tipoitem_id == 1)
+                @if ($item->tipoitem_id == 2)
                 <li class="bg-white p-2 rounded mt-1 border-b border-grey cursor-pointer hover:bg-grey-lighter">
-                    {{$item->description}} {{-- || {{$item->tipoitem_id}} <br> --}}
+                    {{$item->description}}
                 </li>
                 @endif
 
-            @endforeach     
+            @endforeach
             </ol>
-        </div>
-    @else
-        no hay fortalezas :D
-    @endif
+        </div>  
+        @else
+            no hay oportunidades :D
+        @endif
+        
 
 
+        @if ($items->count())
+        <div class="bg-grey-light  w-full  sm:w-1/2 p-4 bg-indigo-100">
+            <div class="flex justify-between py-1">
+                <h3 class="text-sm">
+                    Debilidades de {{$project->name}}
+                </h3>
+            </div>
+            <ol class="text-sm mt-2">
+            @foreach ( $items as $item)
 
-    @if ($items->count())
-    <div class="rounded bg-grey-light flex-no-shrink w-full  sm:w-1/2 p-4 my-4 bg-red-100">
-        <div class="flex justify-between py-1">
-            <h3 class="text-sm">
-                Oportunidades de {{$project->name}}
-            </h3>
-        </div>
-        <ol class="text-sm mt-2">
-        @foreach ( $items as $item)
+                @if ($item->tipoitem_id == 3)
+                <li class="bg-white p-2 rounded mt-1 border-b border-grey cursor-pointer hover:bg-grey-lighter">
+                    {{$item->description}}
+                </li>
+                @endif
 
-            @if ($item->tipoitem_id == 2)
-            <li class="bg-white p-2 rounded mt-1 border-b border-grey cursor-pointer hover:bg-grey-lighter">
-                {{$item->description}}
-            </li>
-            @endif
-
-        @endforeach
-        </ol>
-    </div>  
-    @else
-        no hay oportunidades :D
-    @endif
-    
+            @endforeach  
+            </ol>
+        </div>  
+        @else
+            no hay debilidades, somos muy fuertes :D
+        @endif
 
 
-    @if ($items->count())
-    <div class="rounded bg-grey-light flex-no-shrink w-full  sm:w-1/2 p-4 my-4 bg-indigo-100">
-        <div class="flex justify-between py-1">
-            <h3 class="text-sm">
-                Debilidades de {{$project->name}}
-            </h3>
-        </div>
-        <ol class="text-sm mt-2">
-        @foreach ( $items as $item)
+        @if ($items->count())
+        <div class=" bg-grey-light  w-full  sm:w-1/2 p-4  bg-green-100">
+            <div class="flex justify-between py-1">
+                <h3 class="text-sm">
+                    Amenazas de {{$project->name}}
+                </h3>
+            </div>
+            <ol class="text-sm mt-2">
+            @foreach ( $items as $item)
 
-            @if ($item->tipoitem_id == 3)
-            <li class="bg-white p-2 rounded mt-1 border-b border-grey cursor-pointer hover:bg-grey-lighter">
-                {{$item->description}}
-            </li>
-            @endif
+                @if ($item->tipoitem_id == 4)
+                <li class="bg-white p-2 rounded mt-1 border-b border-grey cursor-pointer hover:bg-grey-lighter">
+                    {{$item->description}}
+                </li>
+                @endif
 
-        @endforeach  
-        </ol>
-    </div>  
-    @else
-        no hay debilidades, somos muy fuertes :D
-    @endif
+            @endforeach 
+            </ol>
+        </div>   
+        @else
+            nadie nos amenaza, nosotros somos la amennaza :D
+        @endif
 
 
-    @if ($items->count())
-    <div class="rounded bg-grey-light flex-no-shrink w-full  sm:w-1/2 p-4 my-4 bg-green-100">
-        <div class="flex justify-between py-1">
-            <h3 class="text-sm">
-                Amenazas de {{$project->name}}
-            </h3>
-        </div>
-        <ol class="text-sm mt-2">
-        @foreach ( $items as $item)
+    </div>
 
-            @if ($item->tipoitem_id == 4)
-            <li class="bg-white p-2 rounded mt-1 border-b border-grey cursor-pointer hover:bg-grey-lighter">
-                {{$item->description}}
-            </li>
-            @endif
-
-        @endforeach 
-        </ol>
-    </div>   
-    @else
-        nadie nos amenaza, nosotros somos la amennaza :D
-    @endif
-
-    <br><br>
 
 
 </section>
